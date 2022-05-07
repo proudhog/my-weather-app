1baffab887c61f3eda14ddea7c63ae90d5121d73
proudhog <wiener.blume@gmail.com>
2022-05-07 14:06:10 +0200

    Updated an HTML file

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
+//Current day and time
 
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let currentTime = document.getElementById("current-time");
+currentTime.innerHTML = `${day}, ${hours}:${minutes}`;
+
+// Enter real life data
+
+function showTemperature(response) {
+  console.log(response.data);
+  document.querySelector(".city").innerHTML = response.data.name;
+  let temp = document.querySelector(".number");
+  temp.innerHTML = Math.round(response.data.main.temp);
+  let wind = document.querySelector("#wind-speed");
+  wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity: ${Math.round(response.data.main.humidity)}%`;
+}
+
+function search(city) {
+  let apiKey = "9cd318b691ec86b36d6f703b911de8c0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function showCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+
+let searchForm = document.querySelector("#searching-bar");
+searchForm.addEventListener("submit", showCity);
+search("Berlin");
+
+//Current Location Button
+function searchLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "9cd318b691ec86b36d6f703b911de8c0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-btn");
+currentLocationButton.addEventListener("click", getCurrentLocation);
